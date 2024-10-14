5d7e1c51604493d99c2ca232b0226f766b631b89
Jonathan Borthwick <jborthwickog@gmail.com>
2024-10-14 16:40:54 -0700

    Fix to cors issue; needed to import it! Also, fix to static fiel location; I told it web but the folder was prototype

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require('express');
+const cors = require('cors');  // Import the cors package
 const path = require('path');
 const app = express();
 const port =  process.env.PORT ||3000;
 //Has the guts of Big Sister App
 const dailyMessages = require('./routes/dailyMessages');
-app.use(cors());
+
 //JSON middleware
 app.use(express.json());
 
 // Middleware to serve static files
-app.use(express.static(path.join(__dirname, 'clients/web')));
+app.use(express.static(path.join(__dirname, 'clients/prototype')));
+
+app.use(cors());
 
 // Default route for root URL
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'clients/web/index.html'));
+    res.sendFile(path.join(__dirname, 'clients/prototype/index.html'));
 });
 
 /*
